a3c70de5f8f5303be9d91765b560d4a59c5721bf
shijieqin <shijieqin@shijieqindeMacBook-Air.local>
2025-03-11 22:05:20 +0800

    add

---
+++ code/Readme.c
@@ -0,0 +1,9 @@
+Line 18: Order of Taylor expansion
+Line 19: Multiple precision Ns
+Line 29: Number of used CPUs
+Line 511: Forcing scale n_k
+Line 512: Reynolds number Re
+Line 524: Time step-size h
+Line 525: Time interval of the whole simulation
+Line 526: Time interval of output
+Line 707: Initial condition
